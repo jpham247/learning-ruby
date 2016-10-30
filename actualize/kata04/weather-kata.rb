cf21a6293f73e543293b019ff9b2c8df45433296
Jonathan Pham <jonpham247@gmail.com>
2016-10-30 14:19:01 -0700

    actualize : kata-04 exercise from 10.30

---
+++ actualize/kata04/weather-kata.rb
@@ -0,0 +1,17 @@
+# In weather.dat you’ll find daily weather data for Morristown, NJ for June 2002. Download this text file, then write a program to output the day number (column one) with the smallest temperature spread (the maximum temperature is the second column, the minimum the third column).
+
+# Read in 'Weather.dat' File into array. where index  0 is the heading data.
+if File.exists("Weather.dat")
+  
+end
+# use heading data to form hash keys look up table based on space delimited index.
+
+# Go through each row of the weather data except first row to write that row's data into hash object.
+
+# Add Hash object to Parsed Data Array.
+
+# Initialize Variable to keep track of index with the smallest temperature spread. 
+# Loop through WeatherData Array to determine Temperature spread of each row, Set tracking index variable if determined
+# Spread is smaller than currently set value.
+
+# Output the ID of the data index with the smallest spread.
