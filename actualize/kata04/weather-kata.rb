689498fb587c8d9e19ea7a34f6058e5ab8610b19
Jonathan Pham <jonpham247@gmail.com>
2016-10-31 17:12:35 -0700

    actualize : kata04 finished.

--- actualize/kata04/weather-kata.rb
+++ actualize/kata04/weather-kata.rb
@@ -1,56 +1,63 @@
-# In weather.dat you’ll find daily weather data for Morristown, NJ for June 2002. Download this text file, then write a program to output the day number (column one) with the smallest temperature spread (the maximum temperature is the second column, the minimum the third column).
+# In weather.dat you’ll find daily weather data for Morristown, NJ for June 2002. 
+# Download this text file, then write a program to output the day number (column one) 
+# with the smallest temperature spread (the maximum temperature is the second column, 
+# the minimum the third column).
 
-# Read in 'Weather.dat' File into array. where index  0 is the heading data.
-file_name = "weather.dat"
+################## Support Methods ##################
 
-contents_array = Array.new
-if File.exists?(file_name)
-  contents_array = File.readlines(file_name) 
-end
-
-#####  Test ######
-# index = 0
-# contents_array.each do |line|
-#   print index
-#   print line
-#   index += 1
-# end
-####################
-
-# use heading data to form hash keys look up table based on space delimited index.
-index = 0 
-weather_data = Array.new
-header_array = Array.new
-# Using Each contentArray Index other than '0', create and store a new hash using the headerArray for keys. 
-
-contents_array.each do |row|
-  if (index == 0)
-    header_array = row.split # Returns Array of each Heading Column Item
-    # puts header_array.inspect
+def get_line_array_from_file(filename)
+  contents_array = Array.new
+  if File.exists?(filename)
+    contents_array = File.readlines(filename) 
   else
-    column_index = 0
-    header_hash = Hash.new
-
-    # Go through each row of the weather data except first row to write that row's data into hash object.
-    data_row = row.split # Returns Array of Data Row Items
-    # puts data_row.inspect # [TEST]
-    
-    data_row.each do |column_item|
-      # Get Hash Key from 'headerArray'
-      key = header_array.at(column_index).to_s.to_sym
-      # puts "Trying to Add item : #{column_item} with Key : #{key.to_s}" # [TEST]
-      header_hash.store(key,column_item)
-      column_index += 1
-    end
-    # puts header_hash.inspect # [TEST]
-    # Add Hash object to Parsed Data Array.
-    weather_data.push(header_hash)
-
+    puts "ERROR: File (#{filename}), Does Not Exist."
   end
-  index += 1 # Increment Contents_Array index
+  return contents_array
 end
 
-# puts weather_data[0].inspect
+def extract_data_hashes_from_table_array(table_array)
+  index = 0 
+  table_data = Array.new
+  header_array = Array.new
+  # Using Each contentArray Index other than '0', create and store a new hash using the headerArray for keys. 
+
+  table_array.each do |row|
+    if (index == 0)
+      header_array = row.split # Returns Array of each Heading Column Item
+      # puts header_array.inspect
+    else
+      column_index = 0
+      header_hash = Hash.new
+
+      # Go through each row of the weather data except first row to write that row's data into hash object.
+      data_row = row.split # Returns Array of Data Row Items
+      # puts data_row.inspect # [TEST]
+      
+      data_row.each do |column_item|
+        # Get Hash Key from 'headerArray'
+        key = header_array.at(column_index).to_s.to_sym
+        # puts "Trying to Add item : #{column_item} with Key : #{key.to_s}" # [TEST]
+        header_hash.store(key,column_item)
+        column_index += 1
+      end
+      # puts header_hash.inspect # [TEST]
+      # Add Hash object to Parsed Data Array.
+      table_data.push(header_hash)
+
+    end
+    index += 1 # Increment Contents_Array index
+  end
+  return table_data
+end
+
+################## Main Execution ##################
+
+# Read in 'Weather.dat' File into array. Assumes index  0 is the heading data.
+file_name = 'weather.dat'
+# Extract file contents into table array
+file_content = get_line_array_from_file(file_name)
+# use heading data to form hash keys look up table based on space delimited index.
+weather_data = extract_data_hashes_from_table_array(file_content)
 
 # Initialize Variable to keep track of index with the smallest temperature spread. 
 loop_index = 0
@@ -60,24 +67,26 @@ small_temp_day = 0
 
 # Loop through WeatherData Array to determine Temperature spread of each row, Set tracking index variable if determined
 weather_data.each do |data|
-  # puts "Line : 0 #{data.inspect}"
-  loop_index += 1
+  # puts "Line : 0 #{data.inspect}" # TEST
   if (!data.empty?)
     temp_spread = data[:MxT].to_i - data[:MnT].to_i
-    puts "Min Spread for Day : #{data[:Dy]} = #{temp_spread}"
+    # puts "Min Spread for Day : #{data[:Dy]} = #{temp_spread}" # TEST
     # Spread is smaller than currently set value.
     if (temp_spread < min_temp_spread)
       min_temp_spread = temp_spread 
       small_temp_day = data[:Dy]
       index_smallest_temp_spread = loop_index
     end
-
   end
+  loop_index += 1
 end
 
 # Output the ID of the data index with the smallest spread.
-puts "The Weather data with the smallest Temperature spread is:"
-puts "Day : #{small_temp_day} with Temperature Spread of : #{min_temp_spread} Degrees"
+puts 'The Weather data with the smallest Temperature spread is:'
+puts "Day : #{small_temp_day} (index = #{index_smallest_temp_spread}) with Temperature Spread of : #{min_temp_spread} Degrees"
+puts "Index Day = #{weather_data[index_smallest_temp_spread][:Dy]}"
+puts "Index Max Temp = #{weather_data[index_smallest_temp_spread][:MxT]}"
+puts "Index Min Temp = #{weather_data[index_smallest_temp_spread][:MnT]}"
 
 
 
